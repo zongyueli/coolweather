d7c1b389e37eac30d197bb9adfa6f740c8543ee8
zongyueli <18322120404@163.com>
2017-04-12 23:20:29 +0800

    完成遍历省市县三级列表功能。

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.coolweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by zongy on 2017/4/9.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
